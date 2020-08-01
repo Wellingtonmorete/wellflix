f7ed820bf02a99a167bd714c470c26f22437cfc5
Wellingtonmorete <Welllington.morete@gmail.com>
2020-08-01 11:27:57 -0300

    Update: Segunda Aula imersão React: criação de páginas de cadastro video,categoria

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+import PageDefault from "../PageDefault";
+function Pagina404(){
+    return(
+      <PageDefault>
+       <h1>404 not found</h1>
+      </PageDefault>
+    );
+  }
+  
+
+  export default Pagina404;
